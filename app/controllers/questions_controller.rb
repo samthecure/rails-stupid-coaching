bc4eba9c4e7b7ccba66aa2ef4fceed76fab9e7ca
Samuel Magalhaes <samuel.f.magalhaes@gmail.com>
2023-11-16 15:58:08 +0100

    fin 1er app

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,23 @@
+# Question controller
 class QuestionsController < ApplicationController
+
   def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_response(@question)
+  end
+
+  private
+
+  def coach_response(question)
+    if question == 'I am going to work'
+      'Great !'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
